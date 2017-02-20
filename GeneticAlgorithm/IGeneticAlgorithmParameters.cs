64494f9acf3ca019e268b3c47c4985e9605d44bb
tchristc <tom.c.christensen@gmail.com>
2017-02-20 10:17:08 -0600

    Using Jason Bocks code as a base for genetic algorithm

---
+++ GeneticAlgorithm/IGeneticAlgorithmParameters.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GeneticAlgorithm
+{
+    public interface IGeneticAlgorithmParameters<T>
+    {
+        T Copy(T value);
+        ReadOnlyCollection<T> Crossover(ReadOnlyCollection<Chromosome<T>> parents);
+        double FitnessEvaluator(T chromosome);
+        Population<T> GeneratePopulation();
+        T Mutator(T chromosome);
+        ReadOnlyCollection<Chromosome<T>> SelectFittestChildren(Population<T> population);
+        Chromosome<T> Terminator(Population<T> population);
+
+        int ChromosomeLength { get; }
+        double CrossoverProbability { get; }
+        double MutationProbability { get; }
+        int NumberOfGenerations { get; }
+        int NumberOfGenerationRuns { get; }
+        int PopulationSize { get; }
+        int TaskCount { get; }
+        double WorstFitnessValue { get; }
+    }
+}
